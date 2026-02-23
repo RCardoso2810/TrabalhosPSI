4ddb5da38639af1ab56a9ed2c13b4465b768cffd
RCardoso2810 <rodrigoamaralcardo@gmail.com>
2026-02-23 00:57:02 +0000

    Add initial product list and starting cash

---
+++ dados.py
@@ -0,0 +1,26 @@
+# dados.py
+dinheiro_inicial = 150.0  # Caixa inicial
+
+# Produtos (nome, preço, stock)
+produtos = [
+    ("Maçã", 1.0, 10),
+    ("Arroz", 2.0, 10),
+    ("Pizza", 3.5, 10),
+    ("Pão", 0.8, 10),
+    ("Leite", 1.2, 10),
+    ("Queijo", 2.8, 10),
+    ("Carne", 5.0, 10),
+    ("Peixe", 4.5, 10),
+    ("Água", 0.7, 10),
+    ("Sumo", 1.5, 10),
+    ("Refrigerante", 1.8, 10),
+    ("Café", 2.5, 10),
+    ("Batatas Fritas", 1.3, 10),
+    ("Chocolate", 1.6, 10),
+    ("Bolachas", 1.4, 10),
+    ("Champô", 3.2, 10),
+    ("Sabonete", 1.0, 10),
+    ("Pasta de Dentes", 2.2, 10),
+    ("Detergente", 4.0, 10),
+    ("Esponja", 0.9, 10)
+]
