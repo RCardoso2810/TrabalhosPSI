e9b143956f8e8086574162d7ce7deda27f70fcf7
RCardoso2810 <rodrigoamaralcardo@gmail.com>
2026-03-02 00:10:18 +0000

    a

---
+++ aluno.py
@@ -0,0 +1,19 @@
+class Aluno:
+    def __init__(self, nome, turma):
+        self.nome = nome
+        self.turma = turma
+        self.vai_evento = True
+        self.pagamentos = 0.0
+
+    def marcar_presenca(self, vai):
+        self.vai_evento = vai
+
+    def adicionar_pagamento(self, valor):
+        self.pagamentos += valor
+
+    def pagou_total(self, preco_evento):
+        return self.pagamentos >= preco_evento
+
+    def __str__(self):
+        status = "Vai" if self.vai_evento else "Não vai"
+        return f"{self.nome} ({self.turma}) - {status} - Pago: {self.pagamentos}€"
